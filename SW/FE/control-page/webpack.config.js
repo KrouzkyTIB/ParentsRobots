cd3e40d6a64d3f4c3d06055a3d455357e73c0e48
Jiří Štengl <stengljirka@gmail.com>
2022-09-10 11:09:22 +0200

    front end build + webpack config

---
+++ SW/FE/control-page/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const fs = require("fs")
+const {getCompiler} = require("ts-loader/dist/compilerSetup");
+const outputDirPath = path.resolve(__dirname, "../../data")
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: outputDirPath
+    },
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
+                    const filePath = outputDirPath + "/" + "bundle.js.LICENSE.txt"
+                    fs.rmSync(filePath)
+                    if (fs.existsSync(filePath)){
+                        console.log(outputDirPath + " removed")
+                    }else{
+                        console.error(outputDirPath + " not removed!")
+                    }
+
+                })
+            }
+        }
+    ]
+};
