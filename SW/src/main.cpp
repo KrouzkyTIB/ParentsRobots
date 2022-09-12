eb81235532d75e6ccb8a8e05946e8de94192559b
Jiří Štengl <stengljirka@gmail.com>
2022-09-12 10:32:01 +0200

    done wifi channel reading

--- SW/src/main.cpp
+++ SW/src/main.cpp
@@ -8,13 +8,13 @@ Motor rightMotor(RIGHT_FORWARDS_PIN, RIGHT_BACKWARDS_PIN, RIGHT_SPEED_PIN);
 Motor leftMotor(LEFT_FORWARDS_PIN, LEFT_BACKWARDS_PIN, LEFT_SPEED_PIN);
 ServerHandler serverHandler;
 
+uint8_t readWifiChannelSettings();
+
 void setup() {
     rightMotor.init();
     leftMotor.init();
     Serial.begin(115200);
-    serverHandler.init(1);
-
-
+    serverHandler.init(readWifiChannelSettings());
 }
 
 void loop() {
@@ -23,6 +23,16 @@ void loop() {
     FrontEndData *data = FrontEndData::getInstance();
     rightMotor.setSpeed(data->getRightMotorPower());
     leftMotor.setSpeed(data->getLeftMotorPower());
+}
+
+
+uint8_t readWifiChannelSettings() {
+    pinMode(WIFI_CHANNEL_SELECTOR1, INPUT_PULLUP);
+    pinMode(WIFI_CHANNEL_SELECTOR2, INPUT_PULLUP);
+    pinMode(WIFI_CHANNEL_SELECTOR3, INPUT_PULLUP);
+    return digitalRead(WIFI_CHANNEL_SELECTOR3) * 4 +
+           digitalRead(WIFI_CHANNEL_SELECTOR2) * 2 +
+           digitalRead(WIFI_CHANNEL_SELECTOR1);
 }
 
 
